e7bde9efd53d5f923eeae7c1ce0421ae6ac68318
daichioidawa <daiti0228neko@outlook.jp>
2020-12-30 16:16:02 +0900

    在庫の取得と購入履歴の取得

--- src/sql_c/SqlCont.java
+++ src/sql_c/SqlCont.java
@@ -10,12 +10,12 @@ import java.sql.Statement;
 public class SqlCont {
 	static Connection con = null;
 	static PreparedStatement stmt = null;
-	static ResultSet rs = null;
+
 	static Statement st=null;
 	public static void login() {
 		try {
 			//Class.forName("com.mysql.cj.jdbc.Driver");
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3306?characterEncoding=UTF-8&serverTimezone=JST", "root", "KILLsqlMe0228jkjk");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/example?characterEncoding=UTF-8&serverTimezone=JST", "root", "KILLsqlMe0228jkjk");
 			st = con.createStatement();
 		} catch (SQLException e) {
 			// TODO 自動生成された catch ブロック
@@ -23,7 +23,7 @@ public class SqlCont {
 		}
 	}
 	public static void showdatabase() {
-
+		ResultSet rs = null;
 		String sql = "show databases;";
 		try {
 			rs=st.executeQuery(sql);
@@ -36,6 +36,44 @@ public class SqlCont {
 			// TODO 自動生成された catch ブロック
 			e.printStackTrace();
 		}
-
+	}
+	public static void gettables() {
+		String sql = "show tables;";
+		ResultSet rs = null;
+		try {
+			rs=st.executeQuery(sql);
+			while(rs.next()) {
+			String location = rs.getString(1);
+			System.out.println
+			(location);
+		}
+		} catch (SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+	}
+	public static ResultSet GetBuyLog() {
+		String sql = "select 品名.品名,購入日,個数 from 購入,品名 where 購入.品名=品名.品名 order by 購入日;";
+		ResultSet rs = null;
+		try {
+			rs=st.executeQuery(sql);
+		} catch (SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+			return null;
+		}
+		return rs;
+	}
+	public static ResultSet GetZaiko() {
+		String sql = "select 品名.品名,SUM(個数) as 在庫 from 購入,品名 where 購入.品名=品名.品名 group by 品名.品名;";
+		ResultSet rs = null;
+		try {
+			rs=st.executeQuery(sql);
+		} catch (SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+			return null;
+		}
+		return rs;
 	}
 }
