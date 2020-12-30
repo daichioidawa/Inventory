22044cce7f09380aa4aa3a452d93be665e7694ff
daichioidawa <daiti0228neko@outlook.jp>
2020-12-30 17:49:25 +0900

    在庫を表示するページを作成

---
+++ src/webP/Zaiko.java
@@ -0,0 +1,75 @@
+package webP;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Zaiko
+ */
+@WebServlet("/Zaiko")
+public class Zaiko extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Zaiko() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");response.setContentType("text/html; charset=UTF-8");
+
+		ResultSet rs=null;
+
+		PrintWriter out=response.getWriter();
+
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>あなたの回答</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h1>在庫を表示</h1>");
+		//表をsqlから作成(from)
+		out.println("<table border=\"1\">");
+		try {
+			while(rs.next()) {
+				out.println("<tr>");
+				out.println("<td>"+rs.getObject(1)+"</td>");
+				out.println("<td>"+rs.getObject(2)+"</td>");
+				out.println("<td>"+rs.getObject(3)+"</td>");
+				out.println("</tr>");
+			}
+		} catch (SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		out.println("</table>");
+		//表をsqlから作成(until)
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
